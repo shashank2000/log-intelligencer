4daf8b36eb1e5ea2a9748d9ffae3012984ac69b3
shashank2000 <sr22@stanford.edu>
2023-05-30 15:42:21 -0700

    removed picture

--- gradio_test.py
+++ gradio_test.py
@@ -12,7 +12,6 @@ demo = gr.Interface(
     fn=make_query, 
     inputs=gr.Textbox(label="Query"), 
     outputs=[gr.Textbox(label="Actual response"), gr.JSON(label="Matched logs"), gr.JSON(label="Previous queries")],
-    title="SearchSmart^TM",
-    css=".gradio-container {background: url('file=headscratcher.png')}"
+    title="LogIntelligencer"
 )
 demo.launch()
